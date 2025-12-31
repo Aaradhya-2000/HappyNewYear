14e14ef7e21cb0846a32fd345a8bda5c2eac89fb
Aaradhya <aaradhyaprnjp62.com>
2025-12-31 11:33:54 +0530

    hello happy new year

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HappyNewYear from "./pages/HappyNewYear";
 
 function App() {
-  return <HappyNewYear />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HappyNewYear />} />
+        <Route path="/happy-new-year" element={<HappyNewYear />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
